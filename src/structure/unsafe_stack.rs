786459d68408cba52bc8e8da68f59b2381cb89f0
FuGangqiang <fu_gangqiang@qq.com>
2019-09-12 16:50:47 +0800

    rewrite unsafe stack using NodePtr

--- src/structure/unsafe_stack.rs
+++ src/structure/unsafe_stack.rs
@@ -1,57 +1,113 @@
-use core::ptr::NonNull;
-
 pub struct Stack<T> {
-    head: Option<NonNull<Node<T>>>,
+    head: NodePtr<T>,
 }
+
+struct NodePtr<T>(*mut Node<T>);
 
 struct Node<T> {
     elem: T,
-    next: Option<NonNull<Node<T>>>,
-}
-
-impl<T> Node<T> {
-    fn new(elem: T) -> Self {
-        Node { elem, next: None }
-    }
-
-    fn into_element(self: Box<Self>) -> T {
-        self.elem
-    }
-}
-
-impl<T> Stack<T> {
-    pub fn new() -> Self {
-        Stack { head: None }
-    }
-
-    pub fn push(&mut self, elem: T) {
-        let mut node = Box::new(Node::new(elem));
-        node.next = self.head;
-        let node = Some(Box::into_raw_non_null(node));
-        self.head = node;
-    }
-
-    pub fn pop(&mut self) -> Option<T> {
-        let node = self.head.map(|node| unsafe {
-            let node = Box::from_raw(node.as_ptr());
-            self.head = node.next;
-            node
-        });
-        node.map(Node::into_element)
-    }
-
-    pub fn peek(&self) -> Option<&T> {
-        unsafe { self.head.as_ref().map(|node| &node.as_ref().elem) }
-    }
-
-    pub fn peek_mut(&mut self) -> Option<&mut T> {
-        unsafe { self.head.as_mut().map(|node| &mut node.as_mut().elem) }
-    }
+    next: NodePtr<T>,
 }
 
 impl<T> Drop for Stack<T> {
     fn drop(&mut self) {
         while let Some(_) = self.pop() {}
+    }
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Stack { head: NodePtr::null() }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        self.head.push(elem)
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.pop()
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.elem()
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.elem_mut()
+    }
+}
+
+impl<T> Clone for NodePtr<T> {
+    fn clone(&self) -> NodePtr<T> {
+        Self(self.0)
+    }
+}
+
+impl<T> Copy for NodePtr<T> {}
+
+impl<T> NodePtr<T> {
+    fn null() -> NodePtr<T> {
+        Self(core::ptr::null_mut())
+    }
+
+    fn is_null(&self) -> bool {
+        self.0.is_null()
+    }
+
+    fn set(&mut self, node: NodePtr<T>) {
+        self.0 = node.0
+    }
+
+    fn new(elem: T) -> Self {
+        let node = Node {
+            elem,
+            next: Self::null(),
+        };
+        Self(Box::into_raw(Box::new(node)))
+    }
+
+    fn elem(&self) -> Option<&T> {
+        if self.is_null() {
+            return None;
+        }
+        unsafe { Some(&(*self.0).elem) }
+    }
+
+    fn elem_mut(&mut self) -> Option<&mut T> {
+        if self.is_null() {
+            return None;
+        }
+        unsafe { Some(& mut(*self.0).elem) }
+    }
+
+    fn next(&self) -> Self {
+        if self.is_null() {
+            return Self::null();
+        }
+        unsafe { (*self.0).next }
+    }
+
+    fn set_next(&mut self, next: NodePtr<T>) {
+        if self.is_null() {
+            return;
+        }
+        unsafe { (*self.0).next = next }
+    }
+
+    fn push(&mut self, elem: T) {
+        let mut node = Self::new(elem);
+        node.set_next(self.clone());
+        self.set(node);
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        if self.is_null() {
+            return None;
+        }
+        let node = unsafe { Box::from_raw(self.0) };
+        let next = self.next();
+        self.set(next);
+        Some(node.elem)
     }
 }
 
